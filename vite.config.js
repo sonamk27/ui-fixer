d15a4af05e8578cf2a05854f9ec57a3d05f14f51
sonamk27 <sonamkorade25@gmail.com>
2026-05-03 11:10:06 +0530

    fix: add vite config and index.html

---
+++ vite.config.js
@@ -0,0 +1,24 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+import path from 'path'
+
+export default defineConfig({
+  plugins: [react()],
+  root: '.',
+  publicDir: 'public',
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, './src'),
+    },
+  },
+  build: {
+    outDir: 'build',
+    sourcemap: true,
+    rollupOptions: {
+      input: './index.html',
+    },
+  },
+  server: {
+    port: 3000,
+  },
+})
