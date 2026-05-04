c9c80cdc683e29566acefa111ba66b7657df35ab
sonamk27 <sonamkorade25@gmail.com>
2026-05-04 20:56:24 +0530

    fix empty index.jsx

--- src/index.jsx
+++ src/index.jsx
@@ -1,2 +1,13 @@
 // React DevTools: install the browser extension from 
 // https://reactjs.org/link/react-devtools for component inspection
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App';
+import './index.css';
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
